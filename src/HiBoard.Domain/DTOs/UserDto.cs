04a372ffd0a2cefa1a3a326f72fd2c7b48fcf129
Lidor Bekel <lidor.bekel@camilyo.com>
2022-04-09 20:35:09 +0300

    Fix(UserDto) : Fix Attributes init

--- src/HiBoard.Domain/DTOs/UserDto.cs
+++ src/HiBoard.Domain/DTOs/UserDto.cs
@@ -1,19 +1,17 @@
-﻿using HiBoard.Domain.Enums;
-
-namespace HiBoard.Domain.DTOs;
+﻿namespace HiBoard.Domain.DTOs;
 
     public class UserDto
     {
         public int Id { get; protected set; }
 
-        public string? UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         public string? FirstName { get; set; } 
 
-        public string? LastName { get; set; }  
+        public string? LastName { get; set; } 
 
-        public UserRole Role { get; set; }
+        public string Role { get; set; } = string.Empty;
 
-        public UserDepartments Department { get; set; }
-}
+        public string Department { get; set; } = string.Empty;
+    }
 
