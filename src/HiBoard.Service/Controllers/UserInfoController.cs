6f236efb81492880ea6ad88509756f47b5ffdcf9
Lidor Bekel <lidor.bekel@camilyo.com>
2022-04-12 16:32:54 +0300

    Feat(UserInfo): UserInfo Implementation

---
+++ src/HiBoard.Service/Controllers/UserInfoController.cs
@@ -0,0 +1,29 @@
+using HiBoard.Application.Services;
+using HiBoard.Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+
+namespace HiBoard.Service.Controllers
+{
+    [ApiController]
+    [Route("api/userinfo")]
+    public class UserInfoController : ControllerBase
+    {
+        private readonly UserInfoService _service;
+
+        public UserInfoController(UserInfoService service)
+        {
+            _service = service;
+        }
+
+        [SwaggerOperation("Get User info")]
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfoAsync(CancellationToken cancellationToken)
+        {
+            var result = await _service.GetUserInfo(cancellationToken);
+            
+            return Ok(result);
+        }
+    }
+}
