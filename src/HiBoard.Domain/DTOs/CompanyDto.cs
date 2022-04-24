69c7885d59f5b2f1fd7bd919df288f2a42b0e4e2
Nitzan Miranda <Nizzan943@gmail.com>
2022-04-24 23:21:56 +0300

    Feat(Solution): Company support

---
+++ src/HiBoard.Domain/DTOs/CompanyDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiBoard.Domain.DTOs
+{
+    public  class CompanyDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Admin { get; set; } = string.Empty;
+    }
+}
---
+++ src/HiBoard.Domain/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace HiBoard.Domain.Models;
+
+public class Company : ModelBase<Company, int>
+{
+    public override int Id { get; protected set; }
+
+    public string Admin { get; set; } = string.Empty;
+
+    [NotMapped]
+    public ICollection<string>? Departments { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
