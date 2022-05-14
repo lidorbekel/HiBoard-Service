8394ed90ef6ad39d6621758bceb3adfe3712b95b
Lidor Bekel <74781212+lidorbekel@users.noreply.github.com>
2022-05-14 18:15:16 +0300

    Remove redundant usings

--- src/HiBoard.Application/Repositories/UserActivitiesRepository.cs
+++ src/HiBoard.Application/Repositories/UserActivitiesRepository.cs
@@ -1,6 +1,56 @@
-namespace HiBoard.Application.Repositories;
+using AutoMapper;
+using HiBoard.Application.CustomExceptions.CompanyExceptions;
+using HiBoard.Domain.DTOs;
+using HiBoard.Domain.Models;
+using HiBoard.Persistence;
 
-public class UserActivitiesRepository
+namespace HiBoard.Application.Repositories
 {
-    
+    public class UserActivitiesRepository
+    {
+        private readonly HiBoardDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserActivitiesRepository(HiBoardDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CompanyDto> CreateCompanyAsync(CompanyDto companyDto,CancellationToken cancellationToken)
+        {
+            var company = _mapper.Map<Company>(companyDto);
+            
+            await _context.Companies.AddAsync(company, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return _mapper.Map<CompanyDto>(company);
+        }
+
+        public async Task<CompanyDto> GetCompanyByIdAsync(int companyId, CancellationToken cancellationToken)
+        {
+            var company = await _context.Companies.FindAsync(new object?[] { companyId }, cancellationToken);
+            if (company == null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            return _mapper.Map<CompanyDto>(company);
+        }
+
+        public async Task<CompanyDto> UpdateCompanyAsync(int companyId, CompanyDto companyDto, CancellationToken cancellationToken)
+        {
+            var company = await _context.Companies.FindAsync(new object?[] { companyId }, cancellationToken);
+            if (company == null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            company = _mapper.Map<Company>(companyDto);
+            _context.Companies.Update(company);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return _mapper.Map<CompanyDto>(company);
+        }
+    }
 }
