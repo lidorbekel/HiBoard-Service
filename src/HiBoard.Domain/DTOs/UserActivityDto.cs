4809d203280a53ce90be13e137f06e8d3b36247a
Nitzan Miranda <Nizzan943@gmail.com>
2022-06-04 18:34:32 +0300

    update userActivityDto

--- src/HiBoard.Domain/DTOs/UserActivityDto.cs
+++ src/HiBoard.Domain/DTOs/UserActivityDto.cs
@@ -1,6 +1,13 @@
+using HiBoard.Domain.Enums;
+
 namespace HiBoard.Domain.DTOs;
 
 public class UserActivityDto
 {
-    
+
+    public int Id { get; set; }
+
+    public ActivityDto? Activity { get; set; }
+
+    public Status Status { get; set; }
 }
