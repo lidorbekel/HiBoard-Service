444ef450d0c531f4420ff865658d8e2cafcc83a9
Nitzan Miranda <Nizzan943@gmail.com>
2022-06-10 15:35:34 +0300

    fix

--- src/HiBoard.Domain/DTOs/UserActivityDto.cs
+++ src/HiBoard.Domain/DTOs/UserActivityDto.cs
@@ -7,9 +7,7 @@ public class UserActivityDto
 
     public int Id { get; set; }
 
-    public ActivityDto? Activity { get; set; }
-
-    public int ActivityId { get; set; }
-
     public Status Status { get; set; }
+    
+    public ActivityDto? Activity { get; set; }
 }
