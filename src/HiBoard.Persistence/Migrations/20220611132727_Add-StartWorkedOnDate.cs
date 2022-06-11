7ae342beaf61ecda67eb69e4798cafff8b4036a7
Nitzan Miranda <Nizzan943@gmail.com>
2022-06-11 17:23:28 +0300

    migration

---
+++ src/HiBoard.Persistence/Migrations/20220611132727_Add-StartWorkedOnDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiBoard.Persistence.Migrations
+{
+    public partial class AddStartWorkedOnDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "started_worked_on",
+                table: "user_activities",
+                type: "DATETIME",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "started_worked_on",
+                table: "user_activities");
+        }
+    }
+}
