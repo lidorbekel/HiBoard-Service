8bff211c0f866b297c1b842211ff378980803fe4
Lidor Bekel <74781212+lidorbekel@users.noreply.github.com>
2022-06-12 19:59:50 +0300

    Add is on time migration

---
+++ src/HiBoard.Persistence/Migrations/20220612165904_add_is_on_time_to_useractivity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiBoard.Persistence.Migrations
+{
+    public partial class add_is_on_time_to_useractivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "is_on_time",
+                table: "user_activities",
+                type: "tinyint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_on_time",
+                table: "user_activities");
+        }
+    }
+}
